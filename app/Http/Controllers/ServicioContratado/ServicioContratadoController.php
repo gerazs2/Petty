216b2cb236e311d67a0b59bce28edd3c44986da1
Omar Jacobo García <omar_192222@live.com>
2020-07-06 15:56:49 -0500

    Se ageregó funcionalidad a ServicioContratadoController
    Se agregó funcionalidad a ServicioContratadoController
    Se agregaron constantes VALIDATION_RULES para la validacion para cad uno de los campos del modelo, ATTTRIBUTES para la pesonalización de los nombres de los atributos del modelo y MESSAGES para la personalización de los mensajes de errores de validacion del modelo del modelo Servicio Contratado

--- app/Http/Controllers/ServicioContratado/ServicioContratadoController.php
+++ app/Http/Controllers/ServicioContratado/ServicioContratadoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\ServicioContratado;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\ServicioContratado;
 
 class ServicioContratadoController extends Controller
 {
@@ -14,7 +15,8 @@ class ServicioContratadoController extends Controller
      */
     public function index()
     {
-        //
+        $serviciosContratados = ServicioContratado::all();
+        return $this->showAll($serviciosContratados, Controller::MESSAGE_OK_, Controller::CODE_OK);
     }
 
     /**
@@ -25,7 +27,34 @@ class ServicioContratadoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validación de los campos recibidos en el Request 
+        $request->validate(
+            ServicioContratado::VALIDATION_RULES,
+            ServicioContratado::MESSAGES,
+            ServicioContratado::ATTRIBUTES
+        );
+
+        // Creación de una nueva instancia de Organización
+        $servicioContratado = new ServicioContratado();
+
+        // Asignación de los campos que sólo pueden ser llenados por el cliente
+        $servicioContratado->nombreServicio = $request->nombreServicio;
+        $servicioContratado->descricpion = $request->descripcion;
+        $servicioContratado->precioBase = $request->precioBase;
+        $servicioContratado->observacinesFinales = $request->observacinesFinales;
+        $servicioContratado->statusServicio = $request->statusServicio;
+        $servicioContratado->costoFinal = $request->costoFinal;
+        $servicioContratado->fechaContrato = $request->fechaContrato;
+        $servicioContratado->fechaTermino = $request->fechaTermino;
+        $servicioContratado->fechaEjecución = $request->fechaEjecución;
+        $servicioContratado->idUsuarioContrato = $request->idUsuarioContrato;
+        $servicioContratado->idMascota = $request->idMascota;
+        $servicioContratado->idSubcategoria = $request->idSubcategoria;
+
+        // guardamos el registro en la DB
+        $servicioContratado->save();
+
+        return $this->success($servicioContratado, Controller::MESSAGE_CREATED, Controller::CODE_CREATED);
     }
 
     /**
@@ -36,7 +65,9 @@ class ServicioContratadoController extends Controller
      */
     public function show($id)
     {
-        //
+        // Usamos el método findOrFail para devolver un error automático en caso de no existir el registro
+        $servicioContatado = ServicioContratado::findOrFail($id);
+        return $this->showOne($servicioContatado, Controller::MESSAGE_OK_, Controller::CODE_OK);
     }
 
     /**
@@ -48,7 +79,68 @@ class ServicioContratadoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // Buscamos si existe el registro en la BDD
+        $servicioContratado = ServicioContratado::findOrFail($id);
+
+        // Validamos si en los campos recibidos en el request se incluyen campos "prohibidos"
+        if ($request->has('id')) {
+            return $this->errorResponse('No se puede actualizar el campo id', Controller::CODE_BAD_REQUEST);
+        }
+
+        // Validamos que los datos recibidos en el Request tengan el formato requerido
+        $request->validate(
+            ServicioContratado::VALIDATION_RULES,
+            ServicioContratado::MESSAGES,
+            ServicioContratado::ATTRIBUTES
+        );
+
+        // Se agregan a la instancia los campos que se hayan recibido en el request
+        if ($request->has('nombreServicio')) {
+            $servicioContratado->nombreServicio = $request->nombreServicio;
+        }
+        if ($request->has('descripcion')) {
+            $servicioContratado->descripcion = $request->descripion;
+        }
+        if ($request->has('precioBase')) {
+            $servicioContratado->precioBase = $request->precioBase;
+        }
+        if ($request->has('observacinesFinales')) {
+            $servicioContratado->observacinesFinales = $request->observacinesFinales;
+        }
+        if ($request->has('statusServicio')) {
+            $servicioContratado->statusServicio = $request->statusServicio;
+        }
+        if ($request->has('costoFinal')) {
+            $servicioContratado->costoFinal = $request->costoFinal;
+        }
+        if ($request->has('fechaContrato')) {
+            $servicioContratado->fechaContrato = $request->fechaContrato;
+        }
+        if ($request->has('fechaTermino')) {
+            $servicioContratado->fechaTermino = $request->fechaTermino;
+        }
+        if ($request->has('fechaEjecucion')) {
+            $servicioContratado->fechaEjecucion = $request->fechaEjecucion;
+        }
+        if ($request->has('idUsuarioContrato')) {
+            $servicioContratado->idUsuarioContrato = $request->idUsuarioContrato;
+        }
+        if ($request->has('idMascota')) {
+            $servicioContratado->idMascota = $request->idMascota;
+        }
+        if ($request->has('idSubCategoria')) {
+            $servicioContratado->idSubCategoria = $request->idSubCategoria;
+        }
+
+        // Comprobamos si la instancia ha tenido algun cambio para ser actualizado
+        if (!$servicioContratado->isDirty()) {
+            return $this->errorResponse('Se debe especificar al menos un campo diferente para actualizar el registro.', Controller::CODE_BAD_REQUEST);
+        }
+
+        // Guardamos el registro en la BDD
+        $servicioContratado->save();
+
+        return $this->success($servicioContratado, Controller::MESSAGE_OK_, Controller::CODE_OK);
     }
 
     /**
@@ -59,6 +151,8 @@ class ServicioContratadoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $servicioContratado = ServicioContratado::findOrFail($id);
+        $servicioContratado->delete();
+        return $this->success($servicioContratado, Controller::MESSAGE_OK_, Controller::CODE_OK);
     }
 }
--- app/ServicioContratado.php
+++ app/ServicioContratado.php
@@ -11,26 +11,79 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ServicioContratado extends Model
-{   
+{
     const STATUS_SERVICIO_TERMINADO = 'Terminado';
     const STATUS_SERVICIO_PENDIENTE = 'Pendiente';
     const STATUS_SERVICIO_CANCELADO = 'Cancelado';
     const STATUS_SERVICIO_RECLAMO = 'Reclamo';
 
+    const MAX_NOMBRE_SERVICIO = 100;
+    const MAX_OBSERVACIONES_FINALES = 250;
+    const MAX_ESTATUS_SERVICIO = 20;
+
+    /**
+     * Strings personalizados para los nombres de los campos
+     */
+    const ATTRIBUTES = [
+        'nombreServicio' => 'Nombre del Servicio',
+        'descripcion' => 'Descripción del Servicio',
+        'precio Base' => 'Precio Base',
+        // TODO Corregir en migraciones
+        'observacinesFinales' => 'Observaciones Finales',
+        'statusServicio' => 'Estatus del Servicio',
+        'costoFinal' => 'Costo Final',
+        'fechaContrato' => 'Fecha de Contrato',
+        'fechaTermino' => 'Fecha de Finalización',
+        'fechaEjecucion' => 'Fecha de Ejecución ',
+        'idUsuarioContrato' => 'Usuario Contratado',
+        'idMascota' => 'Mascota',
+        'idSubcategoria' => 'SubCategoría'
+    ];
+
+    /**
+     * Reglas de validacion para cada uno de los campos de este modelo
+     */
+    const VALIDATION_RULES =  [
+        'nombreServicio' => 'required|string|max:' . ServicioContratado::MAX_NOMBRE_SERVICIO,
+        'descripcion' => 'required|string',
+        'precio Base' => 'required|numeric',
+        'observacinesFinales' => 'required|string|max:' . ServicioContratado::MAX_OBSERVACIONES_FINALES,
+        'statusServicio' => 'required|string|max:' . ServicioContratado::MAX_ESTATUS_SERVICIO,
+        'costoFinal' => 'required|numeric',
+        'fechaContrato' => 'required|date_format:d/m/Y H:i:s',
+        'fechaTermino' => 'required|date_format:d/m/Y H:i:s|after_or_equal:fechaContrato',
+        'fechaEjecucion' => 'required|date_format:d/m/Y H:i:s|after_or_equal:fechaContrato',
+        'idUsuarioContrato' => 'required|integer|exists:usuarios,id',
+        'idMascota' => 'required|integer|exists:mascotas,id',
+        'idSubcategoria' => 'required|integer|exists:subcategorias,id',
+    ];
+    /**
+     * Strings personalizados para los mesajes de error 
+     */
+    const MESSAGES = [
+        'required' => 'El campo :attribute es requerido.',
+        'numeric' => 'El campo :attribute debe ser numérico.',
+        'date_format' => 'El campo :attribute no coinide con el formato :format.',
+        'after_or_equal' => 'La fecha :attribute debe ser después de :date.',
+        'unique' => 'Este campo :attribute ya ha sido registrado.',
+        'exists' => 'El valor de este campo :attribute no existe.',
+        'max' => 'La longitud debe ser de no mas de :max caracteres.',
+    ];
+
     use SoftDeletes;
 
-	//la tabla asociada al modelo
+    //la tabla asociada al modelo
     protected $table = 'serviciosContratados';
 
     //importamos la clase softDeletes para la eliminacion bandera
-	protected $dates = ['deleted_at'];
+    protected $dates = ['deleted_at'];
 
     //los campos que se podran llenar al crear el registro
     protected $fillable = [
-    	'nombreServicio',
+        'nombreServicio',
         'descripcion',
         'precioBase',
-        'observacinesFinales',//asi esta en la BD,
+        'observacinesFinales', //asi esta en la BD,
         'statusServicio',
         'costoFinal',
         'fechaContrato',
@@ -47,8 +100,9 @@ class ServicioContratado extends Model
      * segundo parametro: el nombre de la llave foranea de esta clase que hace referencia al 
                         id del otro modelo.
      */
-    public function usuarioContrato(){
-        return $this->belongsTo(User::class,'idUsuarioContrato');
+    public function usuarioContrato()
+    {
+        return $this->belongsTo(User::class, 'idUsuarioContrato');
     }
 
     /**
@@ -57,8 +111,9 @@ class ServicioContratado extends Model
      * segundo parametro: el nombre de la llave foranea de esta clase que hace referencia al 
                         id del otro modelo.
      */
-    public function mascota(){
-        return $this->belongsTo(Mascota::class,'idMascota');
+    public function mascota()
+    {
+        return $this->belongsTo(Mascota::class, 'idMascota');
     }
 
     /**
@@ -67,8 +122,9 @@ class ServicioContratado extends Model
      * segundo parametro: el nombre de la llave foranea de esta clase que hace referencia al 
                         id del otro modelo.
      */
-    public function subcategoria(){
-        return $this->belongsTo(SubCategoria::class,'idSubcategoria');
+    public function subcategoria()
+    {
+        return $this->belongsTo(SubCategoria::class, 'idSubcategoria');
     }
 
     /**
@@ -78,8 +134,9 @@ class ServicioContratado extends Model
      * segundo parametro: el nombre de la llave foranea del otro modelo que hace referencia al 
      *                  id de este modelo
      */
-    public function calificaciones(){
-        return $this->hasMany(Calificacion::class,'idServicioContratado');
+    public function calificaciones()
+    {
+        return $this->hasMany(Calificacion::class, 'idServicioContratado');
     }
 
     /**
@@ -89,9 +146,8 @@ class ServicioContratado extends Model
      * segundo parametro: el nombre de la llave foranea del otro modelo que hace referencia al 
      *                  id de este modelo
      */
-    public function mensajes(){
-        return $this->hasMany(Mensaje::class,'idServicioContratado');
+    public function mensajes()
+    {
+        return $this->hasMany(Mensaje::class, 'idServicioContratado');
     }
-
-    
 }
