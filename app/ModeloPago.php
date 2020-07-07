2410895228c1ffcecda3aba8ba1551383e5253a6
Omar Jacobo García <omar_192222@live.com>
2020-07-06 21:23:42 -0500

    Se agregó funcionalidad a ModeloPagoController
    Se agregó funcionalidad a ModeloPagoController
    Se agregaron constantes VALIDATION_RULES para la validacion para cad uno de los campos del modelo, ATTTRIBUTES para la pesonalización de los nombres de los atributos del modelo y MESSAGES para la personalización de los mensajes de errores de validacion del modelo del modelo Servicio ModeloPagoController
    Se tienen que revisar las reglas de validacion para el caso de una actualización

--- app/ModeloPago.php
+++ app/ModeloPago.php
@@ -8,9 +8,48 @@ use Illuminate\Database\Eloquent\softDeletes;
 
 class ModeloPago extends Model
 {
-    
-    
-    
+    const MAX_NOMBRE = 50;
+    const MAX_DESCRIPCION = 250;
+    const MAX_DIAS_CORTE = 30;
+    const MAX_DIAS_LIMITE = 30;
+
+    /**
+     * Strings personalizados para los nombres de los campos
+     */
+    const ATTRIBUTES = [
+        'nombre' => 'Nombre Modelo',
+        'descripcion' => 'Descripción',
+        'diasCorte' => 'Días de Corte',
+        'diasLimite' => 'Días Límite de Pago',
+    ];
+
+    /**
+     * Reglas de validacion para cada uno de los campos de este modelo
+     */
+    // TODO Revisar el required cuando es un UPDATE
+    const VALIDATION_RULES =  [
+        'nombre' => 'required|string|max:' . ModeloPago::MAX_NOMBRE,
+        'descripcion' => 'required|string|max:' . ModeloPago::MAX_DESCRIPCION,
+        'diasCorte' => 'required|integer|min:1|max:' . ModeloPago::MAX_DIAS_CORTE,
+        'diasLimite' => 'required|integer|min:1|max:' . ModeloPago::MAX_DIAS_LIMITE,
+    ];
+    /**
+     * Strings personalizados para los mensajes de error 
+     */
+    const MESSAGES = [
+        'required' => 'El campo :attribute es requerido.',
+        'max' => [
+            'numeric' => 'El número no debe ser mayor :max.',
+            'string' => 'La longitud debe ser de no mas de :max caracteres.'
+        ],
+        'min' => [
+            'numeric' => 'El número no debe ser mayor :min.'
+        ],
+
+    ];
+
+
+
     //importamos la clase para la elimiancion virtual
     use softDeletes;
 
@@ -22,11 +61,11 @@ class ModeloPago extends Model
 
     //definimos los campos que se podran ingresar al crear el registro
     protected $fillable = [
-    	'nombre',
-    	'descripcion',
-    	'diasCorte',
-    	'diasLimite'
-	];
+        'nombre',
+        'descripcion',
+        'diasCorte',
+        'diasLimite'
+    ];
 
     /**
      *
@@ -35,7 +74,8 @@ class ModeloPago extends Model
      * segundo parametro: el nombre de la llave foranea del otro modelo que hace referencia al 
      *                  id de este modelo
      */
-    public function organizaciones(){
-        return $this->hasMany(Organizacion::class,'idModeloPago');
+    public function organizaciones()
+    {
+        return $this->hasMany(Organizacion::class, 'idModeloPago');
     }
 }
