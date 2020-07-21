c02d22871be1828cc2806b43f97cd8dc8ee22ef2
Francisco Tapia <panchotapiamiranda@hotmail.com>
2020-07-21 18:36:39 -0500

    Update ServicioController.php
    Si está mal me dicen :'v

--- app/Http/Controllers/Servicio/ServicioController.php
+++ app/Http/Controllers/Servicio/ServicioController.php
@@ -20,6 +20,8 @@ class ServicioController extends Controller
     public function index()
     {
         //
+        $servicio = servicio::all();
+        return $this->showAll($servicio, Controller::MESSAGE_OK_, Controller::CODE_OK );
     }
 
     /**
@@ -31,6 +33,29 @@ class ServicioController extends Controller
     public function store(Request $request)
     {
         //
+        // validamos los campos insertados 
+        $request -> validate(
+        Servicio::VALIDATION_RULES,
+        Servicio::MESSAGES,
+        Servicio::ATTRIBUTES
+        );
+        
+        //Creación de una nueva instancia de Servicio
+        $serv = new Servicio();
+        
+        //Asignación de los campos que solo pueden ser llenados por el cliente
+        
+        $serv->tipoServicio = $request->tipoServicio;
+        $serv->descripcion = $request->descripcion;
+        $serv->tipoServicioEsp = $request->tipoServicioEsp;
+        $serv->especializacion = $request->especializacion;
+        
+        $serv->save();
+        
+        return $this->success($serv,
+                             Controller::MESSAGE_CREATED,
+                             Controller::CODE_CREATED);
+        
     }
 
     /**
@@ -41,7 +66,10 @@ class ServicioController extends Controller
      */
     public function show($id)
     {
-        //
+        //Usamos el método findOrFail para devolver un error automático en caso de no existir el registro
+        $serv-> = Servicio::findOrFail($id);
+        return $this->showOne($serv, 
+                              Controller::MESSAGE_OK_,       Controller::CODE_OK);
     }
 
     /**
@@ -53,7 +81,44 @@ class ServicioController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //Buscamos si existe el registro en la BD}
+        $serv = Servicio::findOrFail($id);
+        
+        //Validamos si en los campos recibidos en el request se incluyen campos "prohibidos"
+        if ($request->has('id')){
+            return $this->errorResponse('No se puede actualizar el campo id', Controller::CODE_BAD_REQUEST);
+        }
+        // Validamos que los datos recibidos en el Request tengan el formato requerido
+        $request->validate(
+            Servicio::VALIDATION_RULES,
+            Servicio::MESSAGES,
+            Servicio::ATTRIBUTES
+        );
+        
+        // Se agregan a la instancia los campos que se hayan recibido en el request
+        if ($request->has('tipoServicio')) {
+            $serv->tipoServicio = $request->tipoServicio;
+        }
+        if ($request->has('descripcion')) {
+            $serv->descripcion = $request->descripcion;
+        }
+        if ($request->has('tipoServicioEsp')) {
+            $serv->tipoServicioEsp = $request->tipoServicioEsp;
+        }
+        if ($request->has('especializacion')) {
+            $consultaMedica->especializacion = $request->especializacion;
+        }
+        
+        if (!$serv->isDirty()) {
+            return $this->errorResponse('Se debe especificar al menos un campo diferente para actualizar el registro.', Controller::CODE_BAD_REQUEST);
+        }
+        
+        // Guardamos el registro en la BDD
+        $serv->save();
+        
+        return $this->success($serv, Controller::MESSAGE_OK_, Controller::CODE_OK);
+        
+        
     }
 
     /**
@@ -65,5 +130,9 @@ class ServicioController extends Controller
     public function destroy($id)
     {
         //
+        $serv = Servicio::findOrFail($id);
+        $serv->delete();
+        return $this->success($serv, Controller::MESSAGE_OK_,
+                             Controller::CODE_OK);
     }
 }
